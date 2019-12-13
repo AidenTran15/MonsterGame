c85753bb1f63537a9595d8163ef9a4279e506f5b
AidenTran15 <aidenkiettran@gmail.com>
2019-12-14 00:39:28 +1100

    [Update] input command from player

--- game.py
+++ game.py
@@ -1,3 +1,14 @@
 player = {'name':'Aiden', 'attack': 10, 'heal': 16, 'health': 100}
 monster = {'name':'Giant', 'attack': 12, 'health': 100}
-print(player['name'])
+print('Please select action')
+print('1> Attack')
+print('2> Heal')
+
+player_choice = input()
+
+if player_choice == '1':
+    print('Attack')
+elif player_choice == '2':
+    print('Heal player')
+else:
+    print('Invalid Input')
